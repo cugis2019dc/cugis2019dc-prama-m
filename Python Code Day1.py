f2b05b643ae3569318b8c3d8d1b97b3b052bf7fa
prama-m <53440520+prama-m@users.noreply.github.com>
2019-07-31 10:10:02 -0400

    Add files via upload

---
+++ Python Code Day1.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Hello my name is Prama
+
+print("Hello my name is Prama")
+
+print("today is Monday")
+
+print(5*2)
+
+print(5/2)
+
+5/2
+5-2
+5**2
+5**2
+(8/9)*3
+"8-0"
+
+print("5*2")
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+
+multiply(4,5)
+
+
+def sum(d,e,f):
+    sum = d+e+f
+    
+    print (sum)
+
+
+sum(3,5,19)
+
+
+def divide(x,y):
+    divide = x/y
+    
+    print(divide)
+    
+    divide(126,2)
+
+def area(a,b):
+    area = .5*a*b
+    
+    print("The area of the triange with base",a, "and height",b, "is", area)
+    
+
+
+area(34,23)
+area(2,3)
+
+
